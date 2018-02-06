b82eb8870f4240fe0a2168652fd1a1090c11607c
EvanSjq <sjq96@foxmail.com>
2018-02-06 19:27:50 +0800

    update user back

--- bookstore_back/src/main/java/zstu/sjq/service/UserService.java
+++ bookstore_back/src/main/java/zstu/sjq/service/UserService.java
@@ -2,10 +2,19 @@ package zstu.sjq.service;
 
 import org.springframework.stereotype.Service;
 
+
 import zstu.sjq.bean.BsUser;
 
 @Service
 public interface UserService {
 
 	public BsUser loginUser(String username,String password);
+	
+	public void addUser(BsUser user);
+	
+	//检测是否有账号
+	public boolean hasUser(String username);
+		
+	//更改账户信息
+	public void updateUser(BsUser user);
 }
