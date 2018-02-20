7bf3647902354d0e49239bf7f715f931b393a129
EvanSjq <sjq96@foxmail.com>
2018-02-20 14:21:55 +0800

    update back

--- bookstore_back/src/main/java/zstu/sjq/controller/UserController.java
+++ bookstore_back/src/main/java/zstu/sjq/controller/UserController.java
@@ -5,12 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,9 +30,9 @@ public class UserController {
 	//登陆
 	@RequestMapping("/loginUser")
 	@ResponseBody
-	public Msg loginUser(String username,String password) {
+	public Msg loginUser(HttpServletRequest request) {
 		
-		BsUser user=userService.loginUser(username, password);
+		BsUser user=userService.loginUser(request.getParameter("username"), request.getParameter("password"));
 		if(user!=null) {
 			return Msg.success().add("user", user);
 		}else {
@@ -41,37 +43,30 @@ public class UserController {
 	//注册
 	@RequestMapping(value="/registerUser",method=RequestMethod.POST)
 	@ResponseBody
-	public Msg registerUser(@Valid BsUser user,BindingResult result) {
-		//BindingResult
-		if(result.hasErrors()) {
-			Map<String,Object> map=new HashMap();
-			List<FieldError> errors = result.getFieldErrors();
-			for(FieldError fieldError:errors) {
-				map.put(fieldError.getField(),fieldError.getDefaultMessage());
-			}
-			return Msg.failed().add("errorFiledMap", map);
-		}else {
-			
-			user.setId(IDUtils.genRandomId());
-			user.setCreated(new Date());
-			user.setUpdated(new Date());
-			userService.addUser(user);
-			
-			return Msg.success();
-		}
+	public Msg registerUser(HttpServletRequest request) {
+		
+		BsUser user=new BsUser();
+		user.setId(IDUtils.genRandomId());
+		user.setUsername(request.getParameter("username"));
+		user.setPassword(request.getParameter("password"));
+		user.setName(request.getParameter("name"));
+		user.setPhone(request.getParameter("phone"));
+		user.setCreated(new Date());
+		user.setUpdated(new Date());
+		
+		userService.addUser(user);
+		
+		return Msg.success();
+		
 	}
 	
 	//后端检验用户名
-	@RequestMapping("/checkName")
+	@RequestMapping("/checkName/{username}")
 	@ResponseBody
-	public Msg checkUsername(String username) {
-		String regx="[a-zA-Z][a-zA-Z0-9]{3,11}";
-		if(!username.matches(regx)) {
-			return Msg.failed().add("va_msg", "用户名必须是4-12位英文和数字组合，且首字母为英文");
-		}
+	public Msg checkUsername(@PathVariable String username) {
 		boolean b=userService.hasUser(username);
 		if(b) {
-			return Msg.success();
+			return Msg.success().add("va_msg", "该用户可以使用");
 		}else {
 			return Msg.failed().add("va_msg", "该用户已经存在");
 		}
@@ -81,19 +76,9 @@ public class UserController {
 	//用户更新信息
 		@RequestMapping("/updateUser")
 		@ResponseBody
-		public Msg update(@Valid BsUser user,BindingResult result) {
-			if(result.hasErrors()) {
-				Map<String,Object> map=new HashMap();
-				List<FieldError> errors = result.getFieldErrors();
-				for(FieldError fieldError:errors) {
-					map.put(fieldError.getField(),fieldError.getDefaultMessage());
-				}
-				return Msg.failed().add("errorFiledMap", map);
-			}else {
-				user.setUpdated(new Date());
-				userService.updateUser(user);
-				return Msg.success();
-			}
+		public Msg update(@Valid BsUser user) {
+			
+			return Msg.success();
 		}
 
 }
