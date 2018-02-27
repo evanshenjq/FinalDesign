44a351d9dc4ce67d0575c4183f21bc96b9a1f7e5
moses.shen <moses.shen@perficient.com>
2018-02-27 16:40:53 +0800

    update front

--- bookstore_web/src/components/UserCartContent.vue
+++ bookstore_web/src/components/UserCartContent.vue
@@ -1,5 +1,18 @@
 <template>
-  
+  <div class="row">
+      <div class="col-md-offset-1" id="cart_title">
+        <h2>购物车</h2>
+      
+        <div class="row" id="sc_list_title">        
+                <div class="col-md-1 col-md-offset-7">价格</div>
+                <div class="col-md-1 col-md-offset-2">数量</div>
+        </div>
+
+        <div class="row" id="sc_list_body">
+            
+        </div>
+      </div>
+  </div>
 </template>
 
 
@@ -29,5 +42,10 @@ export default {
 </script>
 
 <style>
-
+#sc_list_title{
+    border-bottom-style:solid;
+    border-bottom-width: 1px;
+    border-bottom-color: #dddddd;
+    width:90%;
+}
 </style>
