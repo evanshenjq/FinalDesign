b1a65e16904e934888d6829e9bbb0540d0245f84
moses.shen <moses.shen@perficient.com>
2018-02-28 15:39:42 +0800

    update front

--- bookstore_web/src/components/UserCartContent.vue
+++ bookstore_web/src/components/UserCartContent.vue
@@ -9,7 +9,37 @@
         </div>
 
         <div class="row" id="sc_list_body">
-            
+            <div class="row sc_list_item">
+                <div class="col-md-2">
+                    <a>
+                        <img class="sc_list_item_img" src="@/assets/image/1.jpg">
+                    </a>
+                </div>
+                <div class="col-md-5">
+                    <div class="sc_list_item_book">
+                        <a class="sc_list_item_book_name">哈哈哈哈哈</a>
+                        <div class="sc_list_item_book_publish">MC阿红</div>
+                        <div class="sc_list_item_book_stock">库存:300</div>
+                        <a class="sc_list_item_book_del">删除</a>
+                    </div>
+                </div>
+                <div class="col-md-1 sc_list_item_book_price">
+                    ￥13.00
+                </div>
+                <div class="col-md-offset-2 col-md-1 sc_list_item_book_num">
+                    <div class="input-group input-group-sm">
+                        <input type="text" class="form-control" v-model="num">
+                    </div>
+                    <button class="btn btn-warning btn-xs sc_book_update_btn">更新</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="row" id="sc_list_foot">
+            <div class="col-md-offset-8">
+            <span class="sc_list_foot_text">小计 (26 件商品):</span> 
+            <span class="sc_list_foot_total">￥ 2,004.44</span>
+            </div>
         </div>
       </div>
   </div>
@@ -20,11 +50,17 @@
 export default {
     data(){
         return{
-
+            num:1,
         }
     },
     mounted(){
         this.getUserCart();
+    },
+    wathch:{
+        num:function(val){
+            console.log(val);
+            console.log(this);
+        }
     },
     methods:{
         getUserCart(){
@@ -46,6 +82,56 @@ export default {
     border-bottom-style:solid;
     border-bottom-width: 1px;
     border-bottom-color: #dddddd;
-    width:90%;
+    width:85%;
+}
+.sc_list_item{
+    margin-top:10px;
+    padding-bottom: 10px;
+    border-bottom-style:solid;
+    border-bottom-width: 1px;
+    border-bottom-color: #dddddd;
+    width:85%;
+}
+.sc_list_item_img{
+    width: 100px;
+    max-height: 9999px;
+}
+.sc_list_item_book_name{
+    font-weight: 600;
+    font-size: 21px;
+    cursor: pointer;
+}
+.sc_list_item_book{
+    margin-right: 100px;
+}
+.sc_list_item_book_publish{
+    margin-top:6px;
+    line-height: 1.5;
+}
+.sc_list_item_book_stock{
+    margin-top:6px;
+    line-height: 1.5;
+    margin-bottom: 10px;
+}
+.sc_list_item_book_del{
+    cursor: pointer;
+}
+.sc_list_item_book_price{
+    font-size: 18px;
+    color: #B12704;
+    font-weight: 700;
+}
+.sc_book_update_btn{
+    margin-top: 10px;
+    width: 100%;
+}
+.sc_list_foot_text{
+    font-size: 17px;
+    font-weight: 700;
+}
+.sc_list_foot_total{
+    color: #B12704;
+    font-size: 17px;
+    font-weight: 700;
 }
 </style>
--- bookstore_web/src/page/UserOrder.vue
+++ bookstore_web/src/page/UserOrder.vue
@@ -1,10 +1,22 @@
 <template>
-  
+    <div class="row">
+        <app-header/>
+        <login-header/>
+        <app-footer/>
+    </div>
 </template>
 
 <script>
+import AppHeader from '@/common/AppHeader'
+import LoginHeader from '@/common/LoginHeader'
+import AppFooter from '@/common/AppFooter'
+
 export default {
-  
+    components:{
+        AppHeader,
+        LoginHeader,
+        AppFooter
+    }
 }
 </script>
 
