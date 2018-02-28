41e3ebbf623a2e11130a7f974ea34c352a31b11b
moses.shen <moses.shen@perficient.com>
2018-02-28 16:37:47 +0800

    update front

--- bookstore_web/src/components/UserOrderContent.vue
+++ bookstore_web/src/components/UserOrderContent.vue
@@ -2,6 +2,30 @@
   <div class="row">
       <div class="col-md-offset-1">
           <h2>我的订单</h2>
+          <table class="table table-hover order_table">
+            <thead>
+                <tr>
+                    <th>订单号</th>
+                    <th>收件人</th>
+                    <th>电话号码</th>
+                    <th>收货地址</th>
+                    <th>状态</th>
+                    <th>创建时间</th>
+                    <th>查看详情</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
       </div>
   </div>
 </template>
@@ -13,5 +37,7 @@ export default {
 </script>
 
 <style>
-
+.order_table{
+    width: 93%;
+}
 </style>
